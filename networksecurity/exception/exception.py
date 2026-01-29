7b6c8e2975dc7ed8471f6e0f6485e1f915790375
Meet Agrawal <maagrawal2112@gmail.com>
2026-01-29 16:29:47 +0530

    Exception Handling and Logging

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,14 @@
+import sys
+from networksecurity.logging.logger import logging
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        self.error_message=error_message
+        _,_,exc_tb=error_detail.exc_info()
+
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return f"Error occured in script: [{self.file_name}] at line number: [{self.lineno}] error message: [{str(self.error_message)}]"
+
